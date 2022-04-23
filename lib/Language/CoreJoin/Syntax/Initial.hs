e6fa381828ecace780e5d41d8f3389595ebc780d
Gustavo Bicalho <gusbicalho@gmail.com>
2022-04-23 20:02:57 +0000

    avoid extracting Initial syntax contructors

--- lib/Language/CoreJoin/Syntax/Initial.hs
+++ lib/Language/CoreJoin/Syntax/Initial.hs
@@ -6,15 +6,16 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Language.CoreJoin.Syntax.Initial (
-  Process (..),
-  Definition (..),
-  Pattern (..),
+  Process (ProcSend, ProcLocalDef, ProcParallel, ProcInert),
+  Definition (DefReactionRule, DefComposition, DefVoid),
+  Pattern (PatMessage, PatSynchronization),
   Value (..),
   Literal (..),
   Name (..),
@@ -51,56 +52,83 @@ instance
 
 -- | Process
 data Process name where
-  ProcSend :: !(Value name) -> ![Value name] -> Process name
-  ProcLocalDef :: !(Definition name) -> !(Process name) -> Process name
-  ProcParallel :: !(Process name) -> !(Process name) -> Process name
-  ProcInert :: Process name
+  MkProcSend :: !(Value name) -> ![Value name] -> Process name
+  MkProcLocalDef :: !(Definition name) -> !(Process name) -> Process name
+  MkProcParallel :: !(Process name) -> !(Process name) -> Process name
+  MkProcInert :: Process name
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+{-# COMPLETE ProcSend, ProcLocalDef, ProcParallel, ProcInert #-}
+
+pattern ProcSend :: Value name -> [Value name] -> Process name
+pattern ProcSend channel args <- MkProcSend channel args
+pattern ProcLocalDef :: Definition name -> Process name -> Process name
+pattern ProcLocalDef def body <- MkProcLocalDef def body
+pattern ProcParallel :: Process name -> Process name -> Process name
+pattern ProcParallel proc1 proc2 <- MkProcParallel proc1 proc2
+pattern ProcInert :: Process name
+pattern ProcInert <- MkProcInert
 
 instance Syntax.Abstract.Process (InitialSyntax name) (Process name) where
   {-# INLINE send #-}
-  send procVal msgVals = ProcSend procVal (F.toList msgVals)
+  send procVal msgVals = MkProcSend procVal (F.toList msgVals)
 
   {-# INLINE localDef #-}
-  localDef DefVoid p = p
-  localDef def p = ProcLocalDef def p
+  localDef MkDefVoid p = p
+  localDef def p = MkProcLocalDef def p
 
   {-# INLINE parallel #-}
-  parallel ProcInert b = b
-  parallel a ProcInert = a
-  parallel a b = ProcParallel a b
+  parallel MkProcInert b = b
+  parallel a MkProcInert = a
+  parallel a b = MkProcParallel a b
 
   {-# INLINE inert #-}
-  inert = ProcInert
+  inert = MkProcInert
 
 -- | Definition
 data Definition name where
-  DefReactionRule :: !(Pattern name) -> !(Process name) -> Definition name
-  DefComposition :: !(Definition name) -> !(Definition name) -> Definition name
-  DefVoid :: Definition name
+  MkDefReactionRule :: !(Pattern name) -> !(Process name) -> Definition name
+  MkDefComposition :: !(Definition name) -> !(Definition name) -> Definition name
+  MkDefVoid :: Definition name
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable)
 
+{-# COMPLETE DefReactionRule, DefComposition, DefVoid #-}
+
+pattern DefReactionRule :: Pattern name -> Process name -> Definition name
+pattern DefReactionRule pat reaction <- MkDefReactionRule pat reaction
+pattern DefComposition :: Definition name -> Definition name -> Definition name
+pattern DefComposition def1 def2 <- MkDefComposition def1 def2
+pattern DefVoid :: Definition name
+pattern DefVoid <- MkDefVoid
+
 instance Syntax.Abstract.Definition (InitialSyntax name) (Definition name) where
-  void = DefVoid
+  void = MkDefVoid
 
-  compose DefVoid b = b
-  compose a DefVoid = a
-  compose a b = DefComposition a b
+  compose MkDefVoid b = b
+  compose a MkDefVoid = a
+  compose a b = MkDefComposition a b
 
-  reaction = DefReactionRule
+  reaction = MkDefReactionRule
 
 -- | Pattern
 data Pattern name where
-  PatMessage :: !(Name name) -> ![Name name] -> Pattern name
-  PatSynchronization :: !(Pattern name) -> !(Pattern name) -> Pattern name
+  MkPatMessage :: !(Name name) -> ![Name name] -> Pattern name
+  MkPatSynchronization :: !(Pattern name) -> !(Pattern name) -> Pattern name
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+{-# COMPLETE PatMessage, PatSynchronization #-}
+
+pattern PatMessage :: Name name -> [Name name] -> Pattern name
+pattern PatMessage channel args <- MkPatMessage channel args
+pattern PatSynchronization :: Pattern name -> Pattern name -> Pattern name
+pattern PatSynchronization pat1 pat2 <- MkPatSynchronization pat1 pat2
 
 instance
   (Ord name, Show name) =>
   Syntax.Abstract.Pattern (InitialSyntax name) (Pattern name)
   where
-  match procName argNames = PatMessage procName (F.toList argNames)
-  syncAll (pat :| more) = checkDuplicates $ foldr PatSynchronization pat more
+  match procName argNames = MkPatMessage procName (F.toList argNames)
+  syncAll (pat :| more) = checkDuplicates $ foldr MkPatSynchronization pat more
    where
     checkDuplicates :: HasCallStack => Pattern name -> Pattern name
     checkDuplicates fullPat =
@@ -149,26 +177,26 @@ mapValues :: (Value name -> Value name) -> Process name -> Process name
 mapValues f = goProcess
  where
   goProcess = \case
-    p@ProcInert -> p
-    ProcSend procVal argVals ->
-      ProcSend (f procVal) (f <$> argVals)
-    ProcLocalDef def body ->
-      ProcLocalDef (goDefinition def) (goProcess body)
-    ProcParallel p1 p2 ->
-      ProcParallel (goProcess p1) (goProcess p2)
+    p@MkProcInert -> p
+    MkProcSend procVal argVals ->
+      MkProcSend (f procVal) (f <$> argVals)
+    MkProcLocalDef def body ->
+      MkProcLocalDef (goDefinition def) (goProcess body)
+    MkProcParallel p1 p2 ->
+      MkProcParallel (goProcess p1) (goProcess p2)
   goDefinition = \case
-    d@DefVoid -> d
-    DefComposition d1 d2 ->
-      DefComposition (goDefinition d1) (goDefinition d2)
-    DefReactionRule pattern body ->
-      DefReactionRule pattern (goProcess body)
+    d@MkDefVoid -> d
+    MkDefComposition d1 d2 ->
+      MkDefComposition (goDefinition d1) (goDefinition d2)
+    MkDefReactionRule pat body ->
+      MkDefReactionRule pat (goProcess body)
 
 -- Checks
 
 freeVariables :: Ord name => Process name -> Set name
 freeVariables = \case
-  ProcSend name values -> foldMap valueFreeVars (name : values)
-  ProcLocalDef definition body ->
+  MkProcSend name values -> foldMap valueFreeVars (name : values)
+  MkProcLocalDef definition body ->
     let MkDefinitionVariables
           introducedNames
           freeNamesInDefinition = definitionVars definition
@@ -176,9 +204,9 @@ freeVariables = \case
      in Set.union
           freeNamesInDefinition
           (Set.difference freeNamesInBody introducedNames)
-  ProcParallel proc1 proc2 ->
+  MkProcParallel proc1 proc2 ->
     Set.union (freeVariables proc1) (freeVariables proc2)
-  ProcInert -> Set.empty
+  MkProcInert -> Set.empty
  where
   valueFreeVars :: Value name -> Set name
   valueFreeVars = \case
@@ -198,8 +226,8 @@ matchedChannelNames = patternProcessNames . patternVariables
 
 definitionVars :: Ord name => Definition name -> DefinitionVariables name
 definitionVars = \case
-  DefReactionRule pattern body ->
-    let MkPatternVariables{patternProcessNames, patternItemNames} = patternVariables pattern
+  MkDefReactionRule pat body ->
+    let MkPatternVariables{patternProcessNames, patternItemNames} = patternVariables pat
         freeVarsInBody = freeVariables body
      in MkDefinitionVariables
           patternProcessNames
@@ -207,7 +235,7 @@ definitionVars = \case
               freeVarsInBody
               (patternProcessNames <> patternItemNames)
           )
-  DefComposition defA defB ->
+  MkDefComposition defA defB ->
     let (MkDefinitionVariables introducedA freeInA) = definitionVars defA
         (MkDefinitionVariables introducedB freeInB) = definitionVars defB
      in MkDefinitionVariables
@@ -215,7 +243,7 @@ definitionVars = \case
           ( Set.difference freeInA introducedB
               <> Set.difference freeInB introducedA
           )
-  DefVoid -> MkDefinitionVariables Set.empty Set.empty
+  MkDefVoid -> MkDefinitionVariables Set.empty Set.empty
 
 data PatternVariables name = MkPatternVariables
   { patternProcessNames :: !(Set name)
@@ -225,7 +253,7 @@ data PatternVariables name = MkPatternVariables
 
 patternVariables :: Ord name => Pattern name -> PatternVariables name
 patternVariables = \case
-  PatMessage (MkName processName) (coerce -> messageItems) ->
+  MkPatMessage (MkName processName) (coerce -> messageItems) ->
     let multiMessageItemNames = MultiSet.fromList messageItems
         messageItemNames = Set.fromList messageItems
         duplicatesInItemsNames =
@@ -239,7 +267,7 @@ patternVariables = \case
           (Set.singleton processName)
           messageItemNames
           duplicates
-  PatSynchronization patA patB ->
+  MkPatSynchronization patA patB ->
     let ( MkPatternVariables
             procNamesInA
             messageItemNamesInA
